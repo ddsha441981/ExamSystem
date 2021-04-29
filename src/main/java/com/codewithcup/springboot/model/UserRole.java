f7f98baac21ac6a05d280a80795f3bae4c1470e1
ddsha441981 <ddsa44191@gmail.com>
2021-04-29 11:08:08 +0530

    User Entity Created...

---
+++ src/main/java/com/codewithcup/springboot/model/UserRole.java
@@ -0,0 +1,59 @@
+package com.codewithcup.springboot.model;
+
+import javax.persistence.*;
+
+@Entity
+public class UserRole {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long userRoleId;
+//User
+    @ManyToOne(fetch = FetchType.EAGER)
+    private User user;
+    
+    @ManyToOne
+    private Role role;
+
+//    Default Constructor
+
+
+    public UserRole() {
+    }
+
+//    Getter and Setter
+
+    public Long getUserRoleId() {
+        return userRoleId;
+    }
+
+    public void setUserRoleId(Long userRoleId) {
+        this.userRoleId = userRoleId;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+//    ToString
+
+    @Override
+    public String toString() {
+        return "UserRole{" +
+                "userRoleId=" + userRoleId +
+                ", user=" + user +
+                ", role=" + role +
+                '}';
+    }
+}
